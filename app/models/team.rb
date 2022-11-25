07f3290caba10e28d3a205a423ed917ea8f9cdf5
asumi nakajima <nakajima0848@gmail.com>
2022-11-25 13:47:13 +0900

    team, user:モデル、アソシエーションと外部キーの設定

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ApplicationRecord
+  has_many :assigns, dependent: :destroy
+  has_many :users, through: :assigns, source: :user
+
+  belongs_to :owner, class_name: 'User', foreign_key: :owner_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+
+  has_many :assigns, dependent: :destroy
+  has_many :teams, through: :assigns, source: :team
+  has_many :teams, foreign_key: :owner_id
 end
