01d912a8e7a9e061582063df5150b982ce668f2d
asumi nakajima <nakajima0848@gmail.com>
2022-11-28 18:20:26 +0900

    Categoryモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_27_083905) do
+ActiveRecord::Schema.define(version: 2022_11_28_040930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2022_11_27_083905) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["team_id"], name: "index_assigns_on_team_id"
     t.index ["user_id"], name: "index_assigns_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "team_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_categories_on_team_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -79,6 +87,7 @@ ActiveRecord::Schema.define(version: 2022_11_27_083905) do
 
   add_foreign_key "assigns", "teams"
   add_foreign_key "assigns", "users"
+  add_foreign_key "categories", "teams"
   add_foreign_key "tasks", "teams"
   add_foreign_key "tasks", "users"
   add_foreign_key "teams", "users"
