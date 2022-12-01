65b22f773dd90986ec1ed02db0b91cf5b040bb43
asumi nakajima <nakajima0848@gmail.com>
2022-12-01 15:23:34 +0900

    add:ゲスト管理者のログイン機能と、ゲスト管理者のSeedデータの追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,4 +8,9 @@ class Users::SessionsController < Devise::SessionsController
     redirect_to user_path(current_user), notice: 'ゲストユーザーとしてログインしました。'
   end
 
+  def admin_sign_in
+    user = User.admin
+    sign_in user
+    redirect_to user_path(current_user), notice: 'ゲスト管理者ユーザーとしてログインしました。'
+  end
 end
