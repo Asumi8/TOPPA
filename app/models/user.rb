40577ba65265c5d2c559c33c27363159b3a2f24d
asumi nakajima <nakajima0848@gmail.com>
2022-12-08 13:42:34 +0900

    update: バリデーションや日本語化の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 }
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   has_many :assigns, dependent: :destroy
   has_many :team_members, through: :assigns, source: :team
