e31a00da93805f66502508a339ef429ffc618401
asumi nakajima <nakajima0848@gmail.com>
2022-12-08 13:44:01 +0900

    fix: バリデーションが働いていなかった部分を変更

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,22 +1,26 @@
 class Users::InvitationsController < Devise::InvitationsController
   def create
-    if User.find_by(email: params[:user][:email].downcase).present?
+    if params[:user][:email].empty?
+      redirect_to new_user_invitation_path, alert: 'メールアドレスが空白です。入力してください'
+    elsif params[:user][:email] !~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i || (params[:user][:email]).size > 255
+      redirect_to new_user_invitation_path, alert: 'メールアドレスを正しく入力してください'
+    elsif User.find_by(email: params[:user][:email].downcase).present?
       exist_email = params[:user][:email]
-      @team_id = params[:user][:team_id]
+      team_id = params[:user][:team_id]
       user_id = User.where(email: exist_email).pluck(:id)
       user_assigned_teams = Assign.where(id: user_id).pluck(:team_id)
       new_team_assign = []
       new_team_assign << user_assigned_teams
-      new_team_assign << @team_id.to_i
-      @new_team_assign = new_team_assign.flatten
+      new_team_assign << team_id.to_i
+      new_team_assign = new_team_assign.flatten
       user = User.find(user_id[0])
       user.invite!(current_user)
       user.update(team_member_ids: new_team_assign)
       redirect_to teams_path(current_user), notice: "招待メールが#{exist_email}に送信されました。"
     else
       new_email = params[:user][:email]
-      @team_id = params[:user][:team_id]
-      User.invite!(email: new_email, team_member_ids: @team_id)
+      team_id = params[:user][:team_id]
+      User.invite!(email: new_email, team_member_ids: team_id)
       redirect_to teams_path(current_user), notice: "招待メールが#{new_email}に送信されました。"
     end
     #super
